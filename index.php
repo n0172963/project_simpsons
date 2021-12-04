8d3ac5f09f73a7259b21346fa7c6f79220dd6664
n0172963 <nvu@northislandcollege.ca>
2021-12-04 10:47:20 -0500

    Load images based on information from

--- index.php
+++ index.php
@@ -63,99 +63,51 @@
           <div class="characters__container layout-container">
             <div class="characters__row layout-row">
               <ul class="characters__items">
-                <li class="characters__itemContainer">
-                  <div class="characters__item">
-                    <img src="images/homer.png" alt="homer" class="characters__image">
-                    <div class="characters__info">
-                      <h3 class="characters__name"> Homer Simpson </h3>
-                      <div class="characters__age characters__attribute">
-                        <b>Age:</b> 40
-                      </div>
-                      <div class="characters__occupation characters__attribute">
-                        <b>Occupation:</b> Nuclear Safety Inspector
-                      </div>
-                      <div class="characters__voicedBy characters__attribute">
-                        <b>Voiced by:</b> Dan Castellaneta
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li class="characters__itemContainer">
-                  <div class="characters__item">
-                    <img src="images/marge.png" alt="marge" class="characters__image">
-                    <div class="characters__info">
-                      <h3 class="characters__name"> Marge Simpson </h3>
-                      <div class="characters__age characters__attribute">
-                        <b>Age:</b> 40
-                      </div>
-                      <div class="characters__occupation characters__attribute">
-                        <b>Occupation:</b> Housewife
-                      </div>
-                      <div class="characters__voicedBy characters__attribute">
-                        <b>Voiced by:</b> Julie Kavner
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li class="characters__itemContainer">
-                  <div class="characters__item">
-                    <img src="images/bart.png" alt="bart" class="characters__image">
-                    <div class="characters__info">
-                      <h3 class="characters__name"> Bart Simpson </h3>
-                      <div class="characters__age characters__attribute">
-                        <b>Age:</b> 10
-                      </div>
-                      <div class="characters__occupation characters__attribute">
-                        <b>Occupation:</b> Student
-                      </div>
-                      <div class="characters__voicedBy characters__attribute">
-                        <b>Voiced by:</b> Nancy Cartwright
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li class="characters__itemContainer">
-                  <div class="characters__item">
-                    <img src="images/lisa.png" alt="lisa" class="characters__image">
-                    <div class="characters__info">
-                      <h3 class="characters__name"> Lisa Simpson </h3>
-                      <div class="characters__age characters__attribute">
-                        <b>Age:</b> 8
-                      </div>
-                      <div class="characters__occupation characters__attribute">
-                        <b>Occupation:</b> Student
-                      </div>
-                      <div class="characters__voicedBy characters__attribute">
-                        <b>Voiced by:</b> Yeardley Smith
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li class="characters__itemContainer">
-                  <div class="characters__item">
-                    <img src="images/maggie.png" alt="maggie" class="characters__image">
-                    <div class="characters__info">
-                      <h3 class="characters__name"> Maggie Simpson </h3>
-                      <div class="characters__age characters__attribute">
-                        <b>Age:</b> 8
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                <li class="characters__itemContainer">
-                  <div class="characters__item">
-                    <img src="images/moe.png" alt="moe" class="characters__image">
-                    <div class="characters__info">
-                      <h3 class="characters__name"> Moe Szyslak </h3>
-                      <div class="characters__age characters__attribute">
-                        <b>Age:</b> 55
-                      </div>
-                      <div class="characters__occupation characters__attribute">
-                        <b>Occupation:</b> Bartender
-                      </div>
-                    </div>
-                  </div>
-                </li>
+                <?php
+                  foreach ($data as $key => $value){
+                    $firstname = $value['first_name'];
+                    if(!empty($_GET[$key])){
+                      $image = '<img src="'.$value['image_url'].'" alt="'.$value['first_name'].'" class="characters__image">';
+                      $name = '<h3 class="characters__name"> '.$value['first_name'].' '.$value['last_name'].' </h3>';
+                      if(isset($value['age']) !=null || isset($value['age']) != ""){
+                        $age = '<b>Age:</b> '.$value['age'];
+                      }
+                      else{
+                        $age = "";
+                      }
+                      if(isset($value['occupation']) !=null || isset($value['occupation']) != ""){
+                        $job = '<b>Occupation:</b> '.$value['occupation'];
+                      }
+                      else{
+                        $job = "";
+                      }
+                      if(isset($value['voiced_by']) !=null || isset($value['voiced_by'])!=""){
+                        $voice_actor = '<b>Voiced by:</b> '.$value['voiced_by'];
+                      }
+                      else{
+                        $voice_actor = "";
+                      }
+                      echo('
+                      <li class="characters__itemContainer">
+                        <div class="characters__item">
+                          '.$image.'
+                          <div class="characters__info">
+                            '.$name.'
+                            <div class="characters__age characters__attribute">
+                            '.$age.'
+                            </div>
+                            <div class="characters__occupation characters__attribute">
+                            '.$job.'
+                            </div>
+                            <div class="characters__voicedBy characters__attribute">
+                            '.$voice_actor.'
+                            </div>
+                          </div>
+                        </div>
+                      </li>');
+                    }
+                  }
+                ?>
               </ul>
             </div>
           </div>
